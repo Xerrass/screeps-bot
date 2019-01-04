feaaf1a36ae47eca372b13f1ca7c7365eb422b1b
Jonathan Mohs <jonathan.mohs@famohs.de>
2019-01-04 19:22:08 +0100

    Update Var

--- src/main.js
+++ src/main.js
@@ -5,21 +5,21 @@ module.exports.loop = function () {
         // get the creep object
         var creep = Game.creeps[name];
 
-        console.log(name + " is working: " + creep.memory.working);
+        console.log(name + " is full: " + creep.memory.full);
 
         // if creep is bringing energy to the spawn but has no energy left
-        if (creep.memory.working == true && creep.carry.energy == 0) {
+        if (creep.memory.full == true && creep.carry.energy == 0) {
             // switch state
-            creep.memory.working = false;
+            creep.memory.full = false;
         }
         // if creep is harvesting energy but is full
-        else if (creep.memory.working == false && creep.carry.energy == creep.carryCapacity) {
+        else if (creep.memory.full == false && creep.carry.energy == creep.carryCapacity) {
             // switch state
-            creep.memory.working = true;
+            creep.memory.full = true;
         }
 
         // if creep is supposed to transfer energy to the spawn
-        if (creep.memory.working == true) {
+        if (creep.memory.full == true) {
             // try to transfer energy, if the spawn is not in range
             if (creep.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 // move towards the spawn
