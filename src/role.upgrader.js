bfbc8af771ff4f53c0790723c09449eb8683274a
Jonathan Mohs <jonathan.mohs@famohs.de>
2019-01-04 23:34:50 +0100

    0.0.6

--- src/role.upgrader.js
+++ src/role.upgrader.js
@@ -1,6 +1,7 @@
 module.exports = {
     // a function to run the logic for this role
     run: function(creep) {
+        creep.say("Upgrade");
         // if creep is bringing energy to the controller but has no energy left
         if (creep.memory.working == true && creep.carry.energy == 0) {
             // switch state
