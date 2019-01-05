40c9afacff07e9efc0fd9180885f39325964a3c3
Jonathan Mohs <jonathan.mohs@famohs.de>
2019-01-05 04:02:16 +0100

    0.0.98

--- src/role.builder.js
+++ src/role.builder.js
@@ -1,3 +1,5 @@
+var roleUpgrader = require('role.upgrader');
+
 module.exports = {
     // a function to run the logic for this role
     run: function(creep) {
@@ -23,9 +25,17 @@ module.exports = {
         if (creep.memory.working == true) {
             // try to transfer energy, if the spawn is not in range
             var constructionSite = creep.pos.findClosestByPath(FIND_MY_CONSTRUCTION_SITES);
-            if (creep.build(constructionSite) == ERR_NOT_IN_RANGE) {
-                // move towards the spawn
-                creep.moveTo(constructionSite);
+            if (constructionSite != undefined) {
+                // try to build, if the constructionSite is not in range
+                if (creep.build(constructionSite) == ERR_NOT_IN_RANGE) {
+                    // move towards the constructionSite
+                    creep.moveTo(constructionSite);
+                }
+            }
+            // if no constructionSite is found
+            else {
+                // go upgrading the controller
+                roleUpgrader.run(creep);
             }
         }
         // if creep is supposed to harvest energy from source
